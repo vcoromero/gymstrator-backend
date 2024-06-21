186134a5fdad28e263b1157977a8e18dad7720d0
Victor <vcoromero@gmail.com>
2024-06-21 15:09:22 -0600

    base Functions endpoints created

--- src/handlers/functions.go
+++ src/handlers/functions.go
@@ -4,14 +4,75 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/vcoromero/gymstration-backend/config"
 	"github.com/vcoromero/gymstration-backend/src/models"
 )
 
-func GetFunction(w http.ResponseWriter, r *http.Request) {
+func GetFunctions(w http.ResponseWriter, r *http.Request) {
 	var response models.ResponseAPI
 
-	response.Message = "First function endpoint done!"
+	response.Message = "Function retrieved successfully"
+
+	w.Header().Set(config.HeaderContentType, config.ContentTypeJSON)
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetFunction(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if id == "id" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	var response models.ResponseAPI
+
+	response.Message = "Function retrieved successfully"
+
+	w.Header().Set(config.HeaderContentType, config.ContentTypeJSON)
+	json.NewEncoder(w).Encode(response)
+}
+
+func CreateFunction(w http.ResponseWriter, r *http.Request) {
+	var response models.ResponseAPI
+
+	response.Message = "Function created successfully"
+
+	w.Header().Set(config.HeaderContentType, config.ContentTypeJSON)
+	json.NewEncoder(w).Encode(response)
+}
+
+func UpdateFunction(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if id == "id" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	var response models.ResponseAPI
+
+	response.Message = "Function updated successfully"
+
+	w.Header().Set(config.HeaderContentType, config.ContentTypeJSON)
+	json.NewEncoder(w).Encode(response)
+}
+
+func DeleteFunction(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if id == "id" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+
+	var response models.ResponseAPI
+
+	response.Message = "Function deleted successfully"
 
 	w.Header().Set(config.HeaderContentType, config.ContentTypeJSON)
 	json.NewEncoder(w).Encode(response)
